cf31f96875afea2454141ffb1215ca8297b68dc1
PavaniNadupalli <lalithaPavani24893@gmail.com>
2022-12-20 10:11:07 +0530

    first commit

---
+++ GitSample1/src/Sample1/DemoGit1.java
@@ -0,0 +1,20 @@
+package Sample1;
+
+public class DemoGit1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hungry = "yes";
+		if(hungry == "yes")
+		{
+			System.out.println("eat food");
+		}
+		else
+		{
+			System.out.println("enjoy");
+		}
+
+	}
+
+}
