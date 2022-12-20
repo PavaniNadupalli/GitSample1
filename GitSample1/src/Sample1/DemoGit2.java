cb88d2dbb64e57567e900bec7894ce3a322c8102
PavaniNadupalli <lalithaPavani24893@gmail.com>
2022-12-20 12:59:03 +0530

    git2

---
+++ GitSample1/src/Sample1/DemoGit2.java
@@ -0,0 +1,21 @@
+package Sample1;
+
+public class DemoGit2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String hungry = "yes";
+		
+		if(hungry == "yes")
+		{
+			System.out.println("have a nice food");
+		}
+		else
+		{
+			System.out.println("enjoy your day");
+		}
+
+	}
+
+}
